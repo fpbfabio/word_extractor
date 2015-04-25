dd63d91d72f7aec04c270c25ead179d541e486d9
Fabio Benigno <benigno.fabio@gmail.com>
2015-04-25 09:26:47 -0400

    Refactored word_extractor module
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

--- python/word_extractor.py
+++ python/word_extractor.py
@@ -6,8 +6,8 @@ class WordExtractor:
 	QUERY = "*"
 	TARGET_FIELD_NAME = "*"
 	RETURN_LIMIT = 10000000
-	CHARS_TO_IGNORE = [",", ".", "/", "?", ";", ":", "(", ")", "+", "*", "[", "]", "{", "}", "&", "$", "@", "#", "%", "\"", "|", "\\"]
-	HYPHEN = "-"
+	SPECIAL_CHARACTERS = ["'", "-", ",", ".", "/", "?", ";", ":", "(", ")", "+", "*", "[", "]", "{", "}", "&", "$", "@", "#", "%", "\"", "|", "\\"]
+	ENCODING = "utf-8"
 
 	def __init__(self, collection_url, return_field_name):
 		collection_url = collection_url
@@ -17,22 +17,28 @@ class WordExtractor:
 		query = WordExtractor.QUERY
 		self.url = SearchEngine.build_query_url(collection_url, target_field_name, self.return_field_name, return_limit, query)
 
+	def _treat_word(self, word)
+		word = str(word.encode(WordExtractor.ENCODING))[1:].strip("'")
+		word = word.replace("\\", "")
+		for character in WordExtractor.SPECIAL_CHARACTERS:
+			word = word.strip(character)
+			if(character in word):
+				return None
+		if(len(word) <= 1)
+			return None
+		word = word.lower()
+		return word
+
 	def extract_words(self):
 		response = SearchEngine.query(self.url)
 		word_dict = {}
 		for document in response[SearchEngine.DOCUMENTS_KEY]:
-			s = document[self.return_field_name]
-			words = s.split()
-			num_words = len(words)
-			for i in range(0, num_words):
-				valid_word = True
-				words[i] = str(words[i].encode("utf-8"))[1:].strip("'").strip("'").strip("-")
-				for c in WordExtractor.CHARS_TO_IGNORE:
-					words[i] = words[i].strip(c)
-					if(c in words[i]):
-						valid_word = False
-						break
-				words[i] = words[i].lower()
-				if (len(words[i]) > 1 and valid_word):
-					word_dict[words[i]] = word_dict.get(words[i], 0) + 1
-		return word_dict.keys()
+			data = document[self.return_field_name]
+			word_list = data.split()
+			for word in word_list:
+				word = self._treat_word(word)
+				if(word is not None):
+					number_of_ocurrences = word_dict.get(word, 0) + 1
+					word_dict[word] = number_of_ocurrences
+		treated_word_list = word_dict.keys()
+		return treated_word_list
